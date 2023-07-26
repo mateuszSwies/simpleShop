e44b9cd7e435143a30c430edd21b4448ef9e19a2
Mateusz Swies <mateusz1swies@gmail.com>
2023-07-26 21:48:23 +0200

    Create project and add rules for environment

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,35 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+  },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'airbnb',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['@typescript-eslint', 'react-refresh'],
+  // settings: {
+  //   'import/resolver': {
+  //     alias: {
+  //       map: [['@', './src']],
+  //       extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
+  //     },
+  //   },
+  // },
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'react/jsx-filename-extension': 'off',
+    'comma-dangle': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/button-has-type': 'off',
   },
-}
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.tsx';
+import './index.css';
 
+// eslint-disable-next-line spaced-comment, @typescript-eslint/ban-ts-comment
+//@ts-ignore
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
